48cd4889f5b27fa3282c3e1905ca5340444b4d01
Kevyn Hale <kevyn@kevynhale.com>
2019-01-17 16:27:31 -0700

    add piwik tracking

--- kevynhale/src/app/app.component.ts
+++ kevynhale/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatomoInjector } from 'ngx-matomo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private matomoInjector: MatomoInjector) {
+    this.matomoInjector.init('analytics.kevynhale.com/piwik.php', 3)
+  }
   title = 'kevynhale';
 }
